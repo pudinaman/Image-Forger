961bf94ce7863842f08b040983650e46261a6d90
ayush <ayush.raj2019@outlook.com>
2023-10-04 22:44:16 +0530

    js(varialble_created)

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchForm=document.getElementById("search-form");
+const searchBox=document.getElementById("search-box")
+const searchResult=document.getElementById("search-result")
+const searchMoreButton=document.getElementById("show-more-btn")
