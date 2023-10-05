7b973a27835758d2e8e9bb89debbefd79bb71d10
Ayush Mandliya <97592722+AyushMandliya@users.noreply.github.com>
2023-10-05 08:35:02 +0530

    creation of tags

--- script.js
+++ script.js
@@ -9,4 +9,11 @@ let page = 1;
 async function searchImages() {
   keyword = searchBox.value;
 
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
 }
