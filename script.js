1b71c32dca08d262a6bf317083c9fafe5385d849
Ayush Mandliya <97592722+AyushMandliya@users.noreply.github.com>
2023-10-05 08:33:09 +0530

    first commit in js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box")
 const searchResult=document.getElementById("search-result")
 const searchMoreButton=document.getElementById("show-more-btn")
+
+let keyword = "";
+let page = 1;
