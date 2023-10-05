dc8c063d3591e0ac94686f144271da70bedfa659
Ayush Mandliya <97592722+AyushMandliya@users.noreply.github.com>
2023-10-05 08:35:52 +0530

    element created

--- script.js
+++ script.js
@@ -16,4 +16,17 @@ async function searchImages() {
 
   const results = data.results;
 
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+  
+
 }
