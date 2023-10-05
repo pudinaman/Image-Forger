6cee79908144d7232f8b259cf3814308dd504504
Ayush Mandliya <97592722+AyushMandliya@users.noreply.github.com>
2023-10-05 08:41:35 +0530

    commit for all other stuffs in script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
+//Access Key for Unsplesh
 const accessKey = "fww1l5b9Soqifw9YTlCrtHq7Lo-yNM52VkVF0qSIg6U";
 
-const searchForm=document.getElementById("search-form");
-const searchBox=document.getElementById("search-box")
-const searchResult=document.getElementById("search-result")
-const searchMoreButton=document.getElementById("show-more-btn")
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreButton = document.getElementById("show-more-btn");
 
 let keyword = "";
 let page = 1;
 
-let keyword = "";
-let page = 1;
-
+//Async function for Search Images
 async function searchImages() {
-  keyword = searchBox.value;
 
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    keyword = searchBox.value;
 
-  const response = await fetch(url);
-  
-  const data = await response.json();
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
-  const results = data.results;
-  
-  results.map((result) => {
-    const image = document.createElement("img");
-    image.src = result.urls.small;
-    
-    const imageLink = document.createElement("a");
-    imageLink.href = result.links.html;
-    
-    imageLink.target = "_blank";
+    const response = await fetch(url);
+    const data = await response.json();
 
-    imageLink.appendChild(image);
-    searchResult.appendChild(imageLink);
-  });
+    const results = data.results;
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+
+    });
 }
 
+//Search form Added for submition
 searchForm.addEventListener("submit", (e) => {
-  
-  e.preventDefault();
+    
+    e.preventDefault();
 
-  page = 1;
+    page = 1;
 
-  searchImages();
+    searchImages();
 });
