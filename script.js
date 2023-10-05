2ceabd10257beb0689d21faa4729e23b68757749
Ayush Mandliya <97592722+AyushMandliya@users.noreply.github.com>
2023-10-05 08:37:25 +0530

    Access Key of API

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const accessKey = "fww1l5b9Soqifw9YTlCrtHq7Lo-yNM52VkVF0qSIg6U";
+
 const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box")
 const searchResult=document.getElementById("search-result")
 const searchMoreButton=document.getElementById("show-more-btn")
+
+let keyword = "";
+let page = 1;
 
 let keyword = "";
 let page = 1;
@@ -12,21 +17,21 @@ async function searchImages() {
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
   const response = await fetch(url);
+  
   const data = await response.json();
 
   const results = data.results;
-
+  
   results.map((result) => {
     const image = document.createElement("img");
     image.src = result.urls.small;
     
     const imageLink = document.createElement("a");
     imageLink.href = result.links.html;
+    
     imageLink.target = "_blank";
 
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   });
-  
-
 }
