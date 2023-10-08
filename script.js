45a69eca1945bc0ec5dd64cd9ece3809aa87b49e
ayush.trivedi0511@gmail.com <its.trivedi05@h=gmail.com>
2023-10-08 18:03:02 +0530

    made some changes in the paging script

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function searchImages() {
 
     keyword = searchBox.value;
 
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -37,7 +37,7 @@ async function searchImages() {
 
 //Search form Added for submition
 searchForm.addEventListener("submit", (e) => {
-    
+
     e.preventDefault();
 
     page = 1;
