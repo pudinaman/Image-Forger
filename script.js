b636037e283a47d94930ea80834613dbbec34816
ayush.trivedi0511@gmail.com <its.trivedi05@h=gmail.com>
2023-10-08 18:29:44 +0530

    checking if the page number is 1 or not.

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ async function searchImages() {
     const response = await fetch(url);
     const data = await response.json();
 
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+
     const results = data.results;
 
     results.map((result) => {
